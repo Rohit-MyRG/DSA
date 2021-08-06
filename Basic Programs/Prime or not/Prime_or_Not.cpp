619fc88bdedca16d5d142b4de4381a6b2612f2bc
Rohit <rohit.myrg@gmail.com>
2021-08-06 21:19:52 +0530

    Prime_or_not.cpp file added

---
+++ Basic Programs/Prime or not/Prime_or_Not.cpp
@@ -0,0 +1,25 @@
+// Q. To Check The number is prime or not.
+// "The number which devide by 1 and itself only is called prime number."
+
+#include<iostream>
+bool isPrime(int num)
+{
+	for(int i=2; i<=(num/2); i++)
+	{
+		if(num%i==0)
+			return false;
+	}
+	return true;
+}
+int main()
+{
+	std::cout<<"Please enter the Number: ";
+	int num{};
+	std::cin>>num;
+
+	if(isPrime(num))
+		std::cout<<"Number is prime...";
+	else
+		std::cout<<"Number is not prime...";
+	return 0;
+}
