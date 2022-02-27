f63445d46e7426b4d772e457a2e19cb87c07463a
Rohit <rohit.myrg@gmail.com>
2022-02-27 22:22:51 +0530

    Array of Object

--- Array/Q1.java
+++ Array/Q1.java
@@ -1,5 +1,26 @@
-class Q1{
+
+class Test{
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Student arr[] = {
+                new Student(101, "Suyog"),
+                new Student(102, "Rutik"),
+                new Student(103, "Ankush"),
+                new Student(104, "Vaishnavi"),
+                new Student(104, "Jayshree")
+        };
+        
+        for (int i = 0; i < arr.length; i++) {
+             System.out.println(arr[i].roll + "\t"+ arr[i].name);
+        }
+         
+    }
+}
+
+class Student {
+    int roll;
+    String name;
+    public Student(int roll, String name) {
+        this.roll = roll;
+        this.name = name;
     }
 }
