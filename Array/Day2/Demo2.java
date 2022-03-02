4244216e2733e2ef26739a0d962598ee7065c506
Rohit <rohit.myrg@gmail.com>
2022-03-02 23:58:53 +0530

    Day2- Demo2.java added

--- Array/Day2/Demo2.java
+++ Array/Day2/Demo2.java
@@ -1,24 +1,18 @@
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
+class Demo2 {
+    public int[] shuffle(int[] num, int n) {
+        int x[] = new int[n * 2];
 
-public class Demo2 {
+        for (int i = 0, k = 0; i < n; i++, k = k + 2) {
+            x[k] = num[i];
+            x[k + 1] = num[n + i];
+        }
+
+        return x;
+    }
     public static void main(String[] args) {
-
-        Collection arr = new ArrayList();
-        arr.add(10);
-        arr.add(20);
-
-        Iterator itr = arr.iterator();
-
-        System.out.println(itr.next());
-        System.out.println(itr.next());
-
-
+        int[] arr = new Demo2().shuffle(new int[] { 2, 5, 1, 3, 4, 7 }, 3);
+        for (int i : arr) {
+            System.out.print(" "+i);
+        }
     }
 }
-
-
-//   collection  -- concept, 
-//   Collection  --  interface
-//   Collections --  class
